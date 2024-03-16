3eaf83ad2eed567b81507f34ff26f6411ce940e6
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2024-03-15 22:34:57 -0300

    feat: add image to page
    add a sample image (added to figma project too)

--- src/com/emerauth/views/landing.cljs
+++ src/com/emerauth/views/landing.cljs
@@ -11,13 +11,21 @@
   (hd/div
     ($ navbar/navbar)
     (hd/div
-      {:class "flex flex-row w-full md:w-3/4 lg:w-2/3 mx-auto"}
+      {:class "flex flex-row w-full md:w-3/4 lg:w-2/3 mx-auto pt-12"}
       (hd/div
-        {:class "w-full border-2 border-red-500"}
-        "guto")
+        {:class "w-full"}
+        (hd/article
+          {:class "flex flex-col space-y-4"}
+          (hd/h1
+            {:class "text-2xl font-bold"}
+            "Hands free, configurable and extensible authentication & authorization.")
+          (hd/p 
+            "Emerauth is a simple and secure way to authenticate your users.")
+          (hd/p "It's a modern, flexible and extensible authentication and authorization library for Clojure and ClojureScript.")))
       (hd/div
-        {:class "w-full border-2 border-red-500"}
-        "josue"))
+        {:class "w-full"}
+        (hd/img
+          {:src "/resources/assets/img/pc.svg"})))
 
     (js/console.error "NAO FAZ MERGE DISSO")
     ($ footer/footer)))
